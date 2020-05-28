93a1035a31fe11eec1f606744a88a4d04fae597a
kwizera emelyne <emekwi50@gmail.com>
2020-05-28 15:22:17 -0500

    first and final commit for assignment 5

---
+++ Decorator/README.txt
@@ -0,0 +1,10 @@
+---> Is the lab completed? Or what percent of the lab is completed?
+     Yes the lab is completed 100%
+---> Time spent :
+     10-15 hours
+---> Major issues, new things learned?
+     button click with timer was quite hard
+     hard to call a variable that is declared on the function block level
+     learnt how to use the function as variable
+---> Any further questions and concerns etc.
+     None so far
---
+++ Decorator/decoratemytext.js
@@ -0,0 +1,66 @@
+//"use strict";
+function clickOnBigDeco(){
+	alert("Hello, world!");
+}
+function changeFont(){
+let textValue = document.getElementById("UItext");
+textValue.className ="increaseText";
+//textValue.style.fontSize ="24pt";
+
+}
+function changePosition(){
+	let textValue = document.getElementById("UItext");
+	textValue.className ="floatText";
+
+}
+function changeBling(){
+	alert("You changed bling");
+	let checkBling = document.getElementById("bling");
+	let textValue = document.getElementById("UItext");
+	if(checkBling.checked){
+		textValue.className ="boldBling";
+	}
+	else{
+		textValue.className ="normalBling";
+	}
+
+}
+
+function increaseExistingSize(){
+	intervalID = setInterval(function(){
+	let textValue = document.getElementById("UItext");
+	changeFont();
+	changePosition();
+	let styleValue= window.getComputedStyle(textValue).getPropertyValue('font-size');
+	textValue.style.fontSize =parseInt(styleValue)+2+"pt" ;}, 2000);
+}
+function removeInterval(){
+	clearInterval(intervalID);
+}
+
+function loadButton(){
+	//when we click on the button do the following
+	let bigDeco=document.getElementById("bigDecol");
+	let clear =document.getElementById("clear");
+
+
+	/*Hello word*/
+	//bigDeco.onclick=clickOnBigDeco;
+
+	/*change the font size to 24pt*/
+	//bigDeco.onclick = changeFont;
+
+	/*change the text position*/
+	//bigDeco.onclick = changePosition;
+
+	/*Increase the textsize and have a timer*/
+	bigDeco.onclick= increaseExistingSize;
+	clear.onclick = removeInterval;
+
+	//when we check the bling box do the following
+
+	let blingChange = document.getElementById("bling");
+	  blingChange.onchange = changeBling;
+}
+
+window.onload = loadButton;
